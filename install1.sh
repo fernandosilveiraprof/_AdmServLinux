5f41d5553f6d37a8def02f648616e432441fa715
Fernando Silveira <53617302+fernandosilveiraprof@users.noreply.github.com>
2021-09-23 20:33:37 -0300

    Create install1.sh

---
+++ install1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+sudo apt update -y &&
+echo "update foi realizado com sucesso " &&
+sudo apt upgrade -y && 
+echo "upgrade foi realizado com sucesso" &&
+sudo apt install apache2 bind9 dnsutils openssh-server openssh-client whois net-tools systemctl git  -y && 
+echo "instalação do "
